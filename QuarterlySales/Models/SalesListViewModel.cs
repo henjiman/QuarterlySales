1f3e5b132f3c1bf24adf7576f2eb3b1f7b948d1b
Ben Hayden <benhayden.cereal@gmail.com>
2023-10-11 12:09:07 -0400

    Add project files.

---
+++ QuarterlySales/Models/SalesListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace QuarterlySales.Models
+{
+    public class SalesListViewModel
+    {
+        public List<Sales> Sales { get; set; }
+        public List<Employee> Employees { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
